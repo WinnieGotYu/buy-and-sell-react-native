c213e1ef636f21d0971d9be0cc346be8e1321bf3
winniegotyu <winniegotyu@gmail.com>
2020-10-03 16:24:05 -0700

    Added url Image

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, Image, SafeAreaView } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, Image, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={3}>Hello WORLD WORLD WORLD </Text>
-      <Image style={styles.meme}source={require("./assets/meme.png")}/>
+      <Image style={styles.meme} source={require("./assets/meme.png")} />
+      <Image
+        source={{
+          width: 200,
+          height: 200,
+          uri: "https://picsum.photos/id/237/200/300"
+        }}
+      />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -18,9 +24,9 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   meme: {
-    height: 400, 
+    height: 400,
     width: "100%",
     // justifyContent: 'center',
     // alignItems: 'center'
-  }
+  },
 });
