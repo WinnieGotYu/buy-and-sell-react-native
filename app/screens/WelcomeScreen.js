aac4bda1ddd28524405264a2ff25fb039e63097d
winniegotyu <winniegotyu@gmail.com>
2020-10-05 15:07:22 -0700

    Added logo

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import { ImageBackground, StyleSheet, View } from 'react-native';
+import React from "react";
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
 
 function WelcomeScreen(props) {
-  return(
-    <ImageBackground source={require("../assets/background.jpg")} style={styles.background}>
-      <View style={styles.loginButton}>
+  return (
+    <ImageBackground
+      source={require("../assets/background.jpg")}
+      style={styles.background}
+    >
+      <View style={styles.logoContainer}>
+        <Image source={require("../assets/logo-red.png")} style={styles.logo} />
+        <Text>Sell What You Don't Need</Text>
       </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.signUpButton}></View>
     </ImageBackground>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   background: {
-    flex: 1, 
-    justifyContent: "flex-end" // Default is "flex-start"
-  }, 
+    flex: 1,
+    justifyContent: "flex-end", // Default is "flex-start"
+    alignItems: "center",
+  },
+  logoContainer:{
+    position: "absolute",
+    top: 90,
+    alignItems: "center"
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
   loginButton: {
-    width: '100%',
+    width: "100%",
     height: 70,
-    backgroundColor: '#fc5c65', 
-  }
-})
+    backgroundColor: "#fc5c65",
+  },
+  signUpButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+});
 
 export default WelcomeScreen;
