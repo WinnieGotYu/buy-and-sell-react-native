c90fa47e69eed3bfd693d1b203c98e3abe432c89
winniegotyu <winniegotyu@gmail.com>
2020-10-05 15:36:34 -0700

    Practicing with borders:

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import React from "react";
-import {
-  StyleSheet,
-  Platform,
-  StatusBar,
-} from "react-native";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+import { View, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    // <WelcomeScreen />
-    <ViewImageScreen />
+    <View style={styles.app}>
+      <View style={styles.container}></View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  app: {
+    flex:1, 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  },
+  container: {
+    backgroundColor: 'dodgerblue', 
+    width: 100, 
+    height: 100,
+
+  }
+})
