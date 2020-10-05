915e07de07c4598c40a5a1740f2cb8e89f60f2c8
winniegotyu <winniegotyu@gmail.com>
2020-10-05 14:54:03 -0700

    Added login button

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { ImageBackground, StyleSheet, View } from 'react-native';
 
 function WelcomeScreen(props) {
   return(
-    <ImageBackground source={require("../assets/background.jpg")} style={styles.background}/>
+    <ImageBackground source={require("../assets/background.jpg")} style={styles.background}>
+      <View style={styles.loginButton}>
+      </View>
+    </ImageBackground>
   )
 }
 
 const styles = StyleSheet.create({
   background: {
-    flex: 1
+    flex: 1, 
+    justifyContent: "flex-end" // Default is "flex-start"
+  }, 
+  loginButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#fc5c65', 
   }
 })
 
