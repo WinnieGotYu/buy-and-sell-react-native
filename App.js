cfc17e2f81fcf5eb28410021089eba95e26e9a9d
winniegotyu <winniegotyu@gmail.com>
2020-10-24 15:27:36 -0700

    Added login button

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
+import AppButton from "./app/components/AppButton";
 
-import AppText from "./app/components/AppText/AppText"
-
+import AppText from "./app/components/AppText";
 
 export default function App() {
   return (
     <View style={styles.app}>
-    <AppText>I Love React Native!</AppText>
+      <AppButton
+        title="Login"
+        onPress={() => {
+          console.log("Tapped Button");
+        }}
+      />
     </View>
   );
 }
